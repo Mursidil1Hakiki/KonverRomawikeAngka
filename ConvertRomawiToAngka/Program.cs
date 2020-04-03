4715fdcbf4dd0f8b514853763c26e616d17214a2
hakiki19 <mdl.hqq@gmail.com>
2020-04-03 18:35:32 +0700

    Add project files.

---
+++ ConvertRomawiToAngka/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertRomawiToAngka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
